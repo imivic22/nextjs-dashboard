b4875591b803487302fbc6101990c3b9b57971f9
imivic22 <kaimi.victor2022@gmail.com>
2025-05-16 09:47:26 -0700

    Authentication
    Added the rest of the authentication code but working to get the credentials functioning correctly

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,21 +1,6 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": [
-      "static/chunks/[root-of-the-server]__4b450952._.js",
-      "static/chunks/a14e7_react-dom_638ad3bb._.js",
-      "static/chunks/node_modules__pnpm_51c25b77._.js",
-      "static/chunks/[root-of-the-server]__49fd8634._.js",
-      "static/chunks/pages__app_5771e187._.js",
-      "static/chunks/pages__app_dca27279._.js"
-    ],
-    "/_error": [
-      "static/chunks/[root-of-the-server]__492eb320._.js",
-      "static/chunks/a14e7_react-dom_638ad3bb._.js",
-      "static/chunks/node_modules__pnpm_51c25b77._.js",
-      "static/chunks/[root-of-the-server]__923cb372._.js",
-      "static/chunks/pages__error_5771e187._.js",
-      "static/chunks/pages__error_424bf430._.js"
-    ]
+    "/_app": []
   },
   "devFiles": [],
   "ampDevFiles": [],
