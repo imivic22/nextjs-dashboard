3e5afda72628aac271f33cf97eef6fd5f150cbb4
Joseph Baranowski <joe@impactmaui.com>
2025-05-16 08:57:06 -1000

    Remove all .gitignore files. Also fix login/logout/auth and install packages

--- auth.ts
+++ auth.ts
@@ -1,32 +1,21 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
-import postgres from 'postgres';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
-import { authConfig } from './auth.config';
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+import bcrypt from 'bcrypt';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
-    console.log('Fetching user with email:', email); // Log email being queried
-    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
-    console.log('User fetched:', user); // Log fetched user
-    return user[0];
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
   } catch (error) {
-    if (error instanceof Error) {
-      console.error('Failed to fetch user:', error.message); // Log error message
-      console.error('Error details:', error); // Log full error details
-    } else {
-      console.error('Unknown error occurred:', error); // Log unknown error
-    }
+    console.error('Failed to fetch user:', error);
     throw new Error('Failed to fetch user.');
   }
 }
 
 export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials) {
@@ -34,19 +23,28 @@ export const { auth, signIn, signOut } = NextAuth({
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-
-          const user = await getUser(email);
-          if (!user) return null;
-
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user;
+        if (!parsedCredentials.success) {
+          return null;
         }
 
-        console.log('Invalid credentials');
-        return null;
+        const { email, password } = parsedCredentials.data;
+        const user = await getUser(email);
+        
+        if (!user) return null;
+        
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        
+        if (!passwordsMatch) return null;
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        };
       },
     }),
   ],
+  pages: {
+    signIn: '/login',
+  },
 });
